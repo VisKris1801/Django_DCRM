0af59de0cb68eab10654199a4318d4ed363d121a
Vishal <vishalkrishna.github@gmail.com>
2023-09-19 10:59:36 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'root',
+)
+
+cursorObject = dataBase.cursor()
+cursorObject.execute("CREATE DATABASE base")
+print("All Done!")
